45ccdd5fce3e73bde245199f121db210c6daf5a6
HenryW7175 <143651504+HenryWu7175@users.noreply.github.com>
2025-04-23 15:52:45 -0400

    added button on top and bottom

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -3,16 +3,15 @@ package edu.temple.counter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.activity.enableEdgeToEdge // Optional: For edge-to-edge display
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.* // Using Material 3 components
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable // For state persistence
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource // To load drawable resources
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,12 +35,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
+    var count by rememberSaveable { mutableStateOf(0) }
     Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+        Button(onClick = { count++ }) {
+            Text(text = "Add")
+        }
         Text(
             text = "0",
             fontSize = 192.sp
         )
+        Button(onClick = { count-- }) {
+            Text(text = "Down")
+        }
     }
 }
 
